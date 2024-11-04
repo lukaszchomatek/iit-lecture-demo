e0d576395efb32a381ee9b09d117cae847175108
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2024-11-04 11:04:27 +0100

    Added function for sorting, changed greeting

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+// add bubble sort on the array
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                std::swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, IT Students!" << std::endl;
     return 0;
 }
